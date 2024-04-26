ee9805a72d0f837acc7477fccd002fb2fd8d3588
ruhulcse <ruhul.cse7862@gmail.com>
2024-04-27 04:31:56 +0600

    notification api done

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ const users = require("./userRoutes");
 const shipment = require("./shipmentRoutes");
 const blogs = require("./blogRoute");
 const guide = require("./guideRoute");
-module.exports = [users, shipment, blogs, guide];
+const notification = require("./notificationRoute");
+module.exports = [users, shipment, blogs, guide, notification];
